99690f034d463fb0dbe342576bebdfc083384859
Roman Marchenko <whisperdepth@gmail.com>
2020-07-19 14:41:06 +0300

    1

--- lesson3/hw1/src/index.js
+++ lesson3/hw1/src/index.js
@@ -1,16 +1,15 @@
 import store from "./store";
 import { setLanguage } from "./language.actions";
-import { addProduct, removeProduct } from './cart.actions';
-import { setUser, removeUser } from './user.actions';
-
+import { addProduct, removeProduct } from "./cart.actions";
+import { setUser, removeUser } from "./user.actions";
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(setLanguage("uk"));
-store.dispatch(addProduct({id: 1, name: "shotgun"}));
-store.dispatch(addProduct({id: 2, name: "katana"}));
+store.dispatch(addProduct({ id: 1, name: "shotgun" }));
+store.dispatch(addProduct({ id: 2, name: "katana" }));
 store.dispatch(removeProduct(1));
-store.dispatch(setUser({id: 1, name: 'Tom'}));
-store.dispatch(removeUser())
+store.dispatch(setUser({ id: 1, name: "Tom" }));
+store.dispatch(removeUser());
